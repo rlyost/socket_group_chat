2993f9195ef0dba880ee7548011385729d98065d
rlyost <ry@ryost.us>
2018-04-08 15:06:32 -0700

    more additions

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function(req, res) {
     if(!session['users']){
         session['users']={};
         session['messages']={};
-        session['msg_id']=0;
+        session['msg_num']=0;
     };
  res.render("index");
 })
@@ -31,31 +31,43 @@ io.sockets.on('connection', function (socket) {
         var new_id = socket.id;
         session.users[new_id] = data.name;
         console.log("Users:", session.users);
+        //Build chat window
         var room_string = '';
-        var user_string = '';
-        user_string = "<div class='d-inline-block border border-dark fader' id='" + new_id + "' style='width: 200px; height: 200px; background-color: white'><p class='d-inline-block text-center' style='width: 200px; height: 50px; background-color: grey'>" + session.users[new_id] + "</p></div>";
-        console.log(user_string);
-        for(var key in session.users) {
-            console.log(key, session.users[key]);
-            room_string += "<div class='d-inline-block border border-dark fader' id='" + key + "' style='width: 200px; height: 200px; background-color: white'><p class='d-inline-block text-center' style='width: 200px; height: 50px; background-color: grey'>" + session.users[key] + "</p></div>";
-        };
+        room_string += "<h3><u>Ninja Talk...</u></h3><div class='d-inline-block border border-dark w-100 p-3' style='height: 600px;' id='chat_win'></div><form class='posting_form' method='post'><div class='form-group mx-auto'><input type='text' placeholder='enter message here...' class='form-control w-100 p-3' id='message' name='message'><input type='hidden' id='userid' name='userid'><input type='submit' class='btn btn-outline-primary btn-lg' value='Send'></div></form>";
         socket.emit( 'create_room', room_string);
-        // socket.broadcast.emit('update_room', msg_string);
+
+        // add joined message
+        var data_msg = "joined the talk"
+        session['msg_num'] += 1;
+        session.messages[session.msg_num] = [data.name, data_msg];
+        msg_string = add_msg(session['msg_num'], data.name, data_msg);
+        socket.broadcast.emit('update_room', msg_string);
+
+        //build messages in chat window
+        for(var key in session.messages) {
+            console.log(key, session.messages[key]);
+            msg_string = add_msg(key, key[0], key[1]);
+            socket.emit('update_room', msg_string);
+        };
     })
     socket.on( "add_new_msg", function(data){
         var msg_string = '';
         var msg_id = socket.id;
         var msg_name = session.users[msg_id];
-        msg_num += 1;
-        session.messages[msg_num] = [msg_name, data.message]
-        console.log(msg_id, msg_name, date.message);
-        msg_string = "<dl class='row d-inline-block' id='" + msg_num + "'><dt class='col-sm-4 d-inline'>" + msg_name + ":</dt><dd class='col-md-12 d-inline'>" + data.message + "</dd></dl>";
+        session['msg_num'] += 1;
+        session.messages[msg_num] = [msg_name, data.message];
+        console.log(msg_id, msg_name, data.message);
+        msg_string = add_msg(session['msg_num'], msg_name, data.message);
         socket.emit('update_room', msg_string);
         socket.broadcast.emit('update_room', msg_string);
     });
     socket.on( "disconnect", function (){
-        delete session.users[socket.id];
         socket.broadcast.emit('left_room', socket.id);
     });
+    //Builds message string
+    function add_msg(id, name, msg){
+        msg_string = "<dl class='row d-inline-block' id='" + id + "'><dt class='col-sm-4 d-inline'>" + name + ":</dt><dd class='col-md-12 d-inline'>" + msg + "</dd></dl>";
+        return msg_string;
+    }
 })
   
