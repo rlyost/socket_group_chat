c558e586de6e77e1eb7a572ca616af229ddb3997
rlyost <ry@ryost.us>
2018-04-09 08:38:46 -0700

    added scrolling new messages, app works

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Import express and path modules.
+var fs = require('fs');
 var express = require( "express");
 var session = require('express-session');
 var path = require( "path");
@@ -27,13 +28,15 @@ io.sockets.on('connection', function (socket) {
     console.log("Client/socket is connected!");
     console.log("Client/socket id is: ", socket.id);
     // all the server socket code goes in here
+
+    //listening for new users - puts them in chat
     socket.on( "add_new_user", function (data){
         var new_id = socket.id;
         session.users[new_id] = data.name;
         console.log("Users:", session.users);
         //Build chat window
         var room_string = '';
-        room_string += "<h3><u>Ninja Talk...</u></h3><div class='d-inline-block border border-dark w-100 p-3' style='height: 600px;' id='chat_win'></div><form class='posting_form' method='post'><div class='form-group mx-auto'><input type='text' placeholder='enter message here...' class='form-control w-100 p-3' id='message' name='message'><input type='hidden' id='userid' name='userid'><input type='submit' class='btn btn-outline-primary btn-lg' value='Send'></div></form>";
+        room_string += "<h3><u>Ninja Talk...</u></h3><div class='d-inline-block border border-dark w-100 p-3' style='height: 600px;' id='chat_win'></div><input type='text' placeholder='message here...' class='form-control w-100 p-3' id='message' name='message'><button id='postmsg' class='btn btn-outline-primary btn-lg'>Send</button>";
         socket.emit( 'create_room', room_string);
 
         // add joined message
@@ -45,29 +48,42 @@ io.sockets.on('connection', function (socket) {
 
         //build messages in chat window
         for(var key in session.messages) {
-            console.log(key, session.messages[key]);
-            msg_string = add_msg(key, key[0], key[1]);
+            console.log(key, session.messages[key][0], session.messages[key][1]);
+            msg_string = add_msg(key, session.messages[key][0], session.messages[key][1]);
             socket.emit('update_room', msg_string);
         };
-    })
+    });
+
+    //listening for new msg -- adds it to the board for all
     socket.on( "add_new_msg", function(data){
         var msg_string = '';
         var msg_id = socket.id;
         var msg_name = session.users[msg_id];
+        console.log("msg_name:", msg_name);
         session['msg_num'] += 1;
-        session.messages[msg_num] = [msg_name, data.message];
+        session.messages[session.msg_num] = [msg_name, data.message];
+        console.log("session.messages[msg_num]:", session.messages[session.msg_num]);
         console.log(msg_id, msg_name, data.message);
         msg_string = add_msg(session['msg_num'], msg_name, data.message);
         socket.emit('update_room', msg_string);
         socket.broadcast.emit('update_room', msg_string);
     });
+
+    // adds left chat message
     socket.on( "disconnect", function (){
-        socket.broadcast.emit('left_room', socket.id);
+        var data_msg = "has left the talk"
+        var msg_string = '';
+        var userid = socket.id;
+        console.log(session.users[userid]);
+        msg_string = add_msg(0, session.users[userid], data_msg);
+        socket.broadcast.emit('update_room', msg_string);
     });
+
     //Builds message string
     function add_msg(id, name, msg){
-        msg_string = "<dl class='row d-inline-block' id='" + id + "'><dt class='col-sm-4 d-inline'>" + name + ":</dt><dd class='col-md-12 d-inline'>" + msg + "</dd></dl>";
+        var msg_string = '';
+        msg_string = "<dl class='row d-block' id='" + id + "'><dt class='col-sm-4 d-inline'>" + name + ":</dt><dd class='col-md-12 d-inline'>" + msg + "</dd></dl>";
         return msg_string;
-    }
-})
+    };
+});
   
